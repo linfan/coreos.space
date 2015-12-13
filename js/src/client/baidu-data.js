0b49b23a0d2f4584228233c1de3c3a65249ed60d
Fan Lin <flin@thoughtworks.com>
2015-12-13 17:53:36 +0800

    Add baidu data js

---
+++ js/src/client/baidu-data.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?e03538261896962b600bb891f4b5adc2";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
